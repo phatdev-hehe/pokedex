a3494ad922bb5b40e866540b96f93ec8071230a1
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-11-12 20:12:39 +0700

    53

--- app/(pokedex)/loading.js
+++ app/(pokedex)/loading.js
@@ -2,11 +2,14 @@ import { CurrentPath } from "@/shared/current-path";
 import { RouterActions } from "@/shared/router-actions";
 import { Callout } from "fumadocs-ui/components/callout";
 import { DocsBody } from "fumadocs-ui/page";
+import { Suspense } from "react";
 
 export default () => (
   <DocsBody>
-    <Callout title="Loading">
-      <CurrentPath />
+    <Callout title="Loading…">
+      <Suspense>
+        <CurrentPath />
+      </Suspense>
       <RouterActions />
     </Callout>
   </DocsBody>
