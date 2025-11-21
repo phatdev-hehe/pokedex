a90898e721d9206d939ada2f8ac44a85018f4c2b
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-11-21 10:39:47 +0700

    104

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,20 +1,38 @@
 import { Pokedex } from "@/(shared)/pokedex-promise-v2";
 import { titleCase } from "@/(shared)/utils";
 import { createSearchAPI } from "fumadocs-core/search/server";
+import fs from "node:fs";
+import path from "node:path";
+
+const filePath = path.join(
+  process.cwd(),
+  "app",
+  "api",
+  "search",
+  "indexes.json"
+);
+
+if (!fs.existsSync(filePath))
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(
+      (
+        await Promise.all(
+          Pokedex.api.types.map(async (apiType) => {
+            const data = await Pokedex.api(apiType, "getList")();
+
+            return data.results.map((item) => ({
+              breadcrumbs: [titleCase(apiType)],
+              title: titleCase(item.name),
+              url: `/${apiType}/${item.name}`,
+            }));
+          })
+        )
+      ).flat()
+    )
+  );
 
 // https://github.com/fuma-nama/fumadocs/blob/304204a38455f103b430bd8ea6fbdcc64ac4ad2f/packages/core/src/search/server.ts#L101
 export const { GET } = createSearchAPI("simple", {
-  indexes: (
-    await Promise.all(
-      Pokedex.api.types.map(async (apiType) => {
-        const data = await Pokedex.api(apiType, "getList")();
-
-        return data.results.map((item) => ({
-          breadcrumbs: [titleCase(apiType)],
-          title: titleCase(item.name),
-          url: `/${apiType}/${item.name}`,
-        }));
-      })
-    )
-  ).flat(),
+  indexes: JSON.parse(fs.readFileSync(filePath, "utf-8")),
 });
