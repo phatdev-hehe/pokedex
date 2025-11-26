5aca3730c7dccca644bd121eae7e32727e54e17d
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-11-26 18:41:41 +0700

    132

--- app/(pokedex)/sitemap.js
+++ app/(pokedex)/sitemap.js
@@ -1,18 +1,30 @@
 import { Pokedex } from "@/(shared)/pokedex-promise-v2";
 
-export default async () =>
-  (
+const withDefaultProps = (values) => ({
+  lastModified: new Date(),
+  changeFrequency: "yearly",
+  ...values,
+});
+
+export default async () => [
+  ...Pokedex.api.groupNames.map((groupName) =>
+    withDefaultProps({
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${groupName}`,
+    })
+  ),
+  ...(
     await Promise.all(
       Pokedex.api.groupNames.map(async (groupName) =>
         (
           await Pokedex.api(groupName, "getList")()
-        ).results.map((item) => ({
-          url: `${
-            process.env.NEXT_PUBLIC_BASE_URL
-          }/${groupName}/${encodeURIComponent(item.name)}`,
-          lastModified: new Date(),
-          changeFrequency: "yearly",
-        }))
+        ).results.map((item) =>
+          withDefaultProps({
+            url: `${
+              process.env.NEXT_PUBLIC_BASE_URL
+            }/${groupName}/${encodeURIComponent(item.name)}`,
+          })
+        )
       )
     )
-  ).flat();
+  ).flat(),
+];
