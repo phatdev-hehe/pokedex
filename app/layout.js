2aa93925d6028e09afd186298bef861583355ab6
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-11-26 18:49:38 +0700

    133

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,10 @@ const font = Geist();
 
 export default ({ children }) => (
   <html lang="en" className={font.className} suppressHydrationWarning>
+    <meta
+      name="google-site-verification"
+      content="0g4xOknHTsS3aWczVHDj-smtRc9MUBhqsnVTeQVAkKE"
+    />
     <body
       // https://fumadocs.dev/docs/ui/manual-installation/next#root-layout
       style={{
