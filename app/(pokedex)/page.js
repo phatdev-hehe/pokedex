328ee56d2f944b005b49788e23cf5c83f3373cda
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-02 14:50:28 +0700

    179

--- app/(pokedex)/page.js
+++ app/(pokedex)/page.js
@@ -1,7 +1,42 @@
+import { Link, table, tabs } from "@/components";
 import { Pokedex } from "@/lib/pokedex-promise-v2";
+import { titleCase } from "@/utils/title-case";
 
-const Page = await Pokedex.createHomePage();
+const title = "Home";
+let [routeCount, pageCount] = [0, 0];
 
-export const { generateMetadata } = Page;
+const content = tabs(
+  ...(await Promise.all(
+    Pokedex.api.groupNames.map(async (groupName) => {
+      const data = await Pokedex.api(groupName, "getList")();
 
-export default Page;
+      ++routeCount;
+      pageCount += data.count;
+
+      return [
+        groupName,
+        table.pagination(data.results, {
+          renderFirstItem: ({ context }) => (
+            <Link href={`/${groupName}/${context.name}`}>
+              {titleCase(context.name)}
+            </Link>
+          ),
+        }),
+      ];
+    })
+  ))
+);
+
+export const generateMetadata = () => ({ title });
+
+export default () => (
+  <Pokedex
+    title={title}
+    descriptions={[
+      ["routes", routeCount],
+      ["pages", pageCount],
+    ]}
+  >
+    {content}
+  </Pokedex>
+);
