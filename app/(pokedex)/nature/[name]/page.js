e9c5614f73067cf2869989d9bad199a08fb427cf
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-04 10:03:59 +0700

    193

---
+++ app/(pokedex)/nature/[name]/page.js
@@ -0,0 +1,84 @@
+import { highlighter, Link, table, tabs } from "@/components";
+import { Pokedex } from "@/lib/pokedex-promise-v2";
+import { titleCase } from "@/utils/title-case";
+
+const Page = await Pokedex.createDetailPage("nature");
+
+export const { generateMetadata, generateStaticParams } = Page;
+
+export default Page(({ context }) => {
+  /** @type Nature */
+  const nature = context.data;
+
+  const decreasedStat = nature.decreased_stat?.name;
+  const hatesFlavor = nature.hates_flavor?.name;
+  const increasedStat = nature.increased_stat?.name;
+  const likesFlavor = nature.likes_flavor?.name;
+
+  return (
+    <>
+      {table(undefined, [
+        [
+          highlighter(
+            "The stat decreased by 10% in Pokémon with this nature.",
+            "stat decreased"
+          ),
+          <Link href={`/stat/${decreasedStat}`}>
+            {titleCase(decreasedStat)}
+          </Link>,
+        ],
+        [
+          highlighter(
+            "The flavor hated by Pokémon with this nature.",
+            "flavor hated"
+          ),
+          <Link href={`/berry-flavor/${hatesFlavor}`}>
+            {titleCase(hatesFlavor)}
+          </Link>,
+        ],
+        [
+          highlighter(
+            "The stat increased by 10% in Pokémon with this nature.",
+            "stat increased"
+          ),
+          <Link href={`/stat/${increasedStat}`}>
+            {titleCase(increasedStat)}
+          </Link>,
+        ],
+        [
+          highlighter(
+            "The flavor liked by Pokémon with this nature.",
+            "flavor liked"
+          ),
+          <Link href={`/berry-flavor/${likesFlavor}`}>
+            {titleCase(likesFlavor)}
+          </Link>,
+        ],
+      ])}
+      {tabs(
+        Page.tabs.names(nature.names),
+        [
+          "move_battle_style_preferences",
+          table.pagination(nature.move_battle_style_preferences, {
+            thead: [undefined, "high_hp_preference", "low_hp_preference"],
+            renderFirstItem: ({ context }) =>
+              titleCase(context.move_battle_style.name),
+            renderItems: ({ context }) => [
+              context.high_hp_preference,
+              context.low_hp_preference,
+            ],
+          }),
+        ],
+        [
+          "pokeathlon_stat_changes",
+          table.pagination(nature.pokeathlon_stat_changes, {
+            thead: [undefined, "max_change"],
+            renderFirstItem: ({ context }) =>
+              titleCase(context.pokeathlon_stat.name),
+            renderItems: ({ context }) => [context.max_change],
+          }),
+        ]
+      )}
+    </>
+  );
+});
---
+++ app/(pokedex)/nature/page.js
@@ -0,0 +1,6 @@
+import { Pokedex } from "@/lib/pokedex-promise-v2";
+
+const Page = await Pokedex.createListPage("nature");
+
+export const { generateMetadata } = Page;
+export default Page;
