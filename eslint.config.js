bb12c37b9920f011c968cf57cee1d11717fa9ca5
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-12 18:36:53 +0700

    249

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+import perfectionist from "eslint-plugin-perfectionist";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  ...nextVitals,
+  perfectionist.configs["recommended-natural"],
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
