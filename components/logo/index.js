7ffe794c483d123a1d483b3f213f24af36ad93a7
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-23 20:29:10 +0700

    320

--- components/logo/index.js
+++ components/logo/index.js
@@ -1,5 +1,11 @@
 import { LazyImage } from "@/components/lazy-image";
 
-import logo from "./2.gif";
+import logo from "./cmon-cat.gif";
 
-export const Logo = ({ size = 40 }) => <LazyImage src={logo} width={size} />;
+export const Logo = ({ size = 30 }) => (
+  <LazyImage
+    src={logo}
+    title={process.env.NEXT_PUBLIC_SITE_TITLE}
+    width={size}
+  />
+);
