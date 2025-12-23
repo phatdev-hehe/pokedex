568053c9af1eb59d891a11a2c253622083f0323a
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-24 00:19:57 +0700

    321

--- components/link/index.js
+++ components/link/index.js
@@ -1,26 +1,42 @@
 "use client";
 
+import { useProgress } from "@bprogress/next";
 import { compact } from "es-toolkit";
 import FumadocsLink from "fumadocs-core/link";
-import { startTransition, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 
-export const Link =
-  // https://nextjs.org/docs/app/getting-started/linking-and-navigating#disabling-prefetching
-  (props) => {
-    const [state, setState] = useState(false);
+const usePrefetchOnHover = (href) => {
+  const router = useRouter();
+  const progress = useProgress();
 
-    return (
-      <FumadocsLink
-        onMouseEnter={() => {
-          startTransition(() => {
-            setState(true);
-          });
-        }}
-        prefetch={state}
-        {...props}
-      />
-    );
-  };
+  const prefetchOnHover = useCallback(() => {
+    progress.start();
+
+    router.prefetch(href, {
+      onInvalidate: prefetchOnHover,
+    });
+
+    progress.stop();
+  }, [router, href]);
+
+  return prefetchOnHover;
+};
+
+export const Link = ({ href, prefetch = false, ...props }) => {
+  const prefetchOnHover = usePrefetchOnHover(href);
+  const onMouseEnter = prefetch ? undefined : prefetchOnHover;
+
+  return (
+    <FumadocsLink
+      href={href}
+      onFocus={onMouseEnter}
+      onMouseEnter={onMouseEnter}
+      prefetch={prefetch}
+      {...props}
+    />
+  );
+};
 
 export const UnnamedLink = ({ href }) => {
   if (href) {
