d3a45c9f049f1aa76e13d576576467f3e80291c7
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-27 23:05:07 +0700

    368

--- app/(pokedex)/[route-name]/layout.js
+++ app/(pokedex)/[route-name]/layout.js
@@ -2,6 +2,8 @@ import { notFound } from "next/navigation";
 
 import { Pokedex } from "@/lib/pokedex-promise-v2";
 
+// export const dynamicParams = false;
+
 export const generateStaticParams = () =>
   Pokedex.api.routeNames.map((routeName) => ({ "route-name": routeName }));
 
