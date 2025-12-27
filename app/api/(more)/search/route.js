7e5c28b604b3452380f584e2d82ff9a3e88c0d81
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-27 21:01:04 +0700

    365

--- app/api/(more)/search/route.js
+++ app/api/(more)/search/route.js
@@ -1,6 +1,6 @@
 import { createSearchAPI } from "fumadocs-core/search/server";
 
-import data from "@/app/api/(more)/local-data/data.json";
+import data from "@/app/api/local-data/data.json";
 import { titleCase } from "@/utils/title-case";
 
 // https://github.com/fuma-nama/fumadocs/blob/304204a38455f103b430bd8ea6fbdcc64ac4ad2f/packages/core/src/search/server.ts#L101
