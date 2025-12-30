31029a70309edc850063342b118442306d72c65f
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-30 07:42:08 +0700

    384

--- app/(pokedex)/[route-name]/page.js
+++ app/(pokedex)/[route-name]/page.js
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 import { list, table } from "@/components";
 import { Link } from "@/components/link";
 import { Pokedex } from "@/lib/pokedex-promise-v2";
@@ -7,32 +9,34 @@ import { titleCase } from "@/utils/title-case";
 export default async ({ params }) => {
   params = await params;
 
-  const title = `${titleCase(`${params["route-name"]} list`)}`;
-  const items = await Pokedex.api(params["route-name"], "rootEndpoint")();
-  const names = items.results.map((item) => item.name);
+  if (Pokedex.api.routeNames.includes(params["route-name"])) {
+    const title = `${titleCase(`${params["route-name"]} list`)}`;
+    const items = await Pokedex.api(params["route-name"], "rootEndpoint")();
+    const names = items.results.map((item) => item.name);
 
-  return (
-    <Pokedex
-      canonical={`/${params["route-name"]}`}
-      descriptions={{
-        count: items.count,
-        links: list.inline(
-          <Link href={`/random/${params["route-name"]}`}>Random</Link>
-        ),
-      }}
-      ogUrl={getOgUrl({
-        title,
-        topic: items.count,
-      })}
-      title={title}
-    >
-      {table.pagination(names, {
-        renderRows: ({ context }) => [
-          <Link href={`/${params["route-name"]}/${context}`}>
-            {titleCase(context)}
-          </Link>,
-        ],
-      })}
-    </Pokedex>
-  );
+    return (
+      <Pokedex
+        canonical={`/${params["route-name"]}`}
+        descriptions={{
+          count: items.count,
+          links: list.inline(
+            <Link href={`/random/${params["route-name"]}`}>Random</Link>
+          ),
+        }}
+        ogUrl={getOgUrl({
+          title,
+          topic: items.count,
+        })}
+        title={title}
+      >
+        {table.pagination(names, {
+          renderRows: ({ context }) => [
+            <Link href={`/${params["route-name"]}/${context}`}>
+              {titleCase(context)}
+            </Link>,
+          ],
+        })}
+      </Pokedex>
+    );
+  } else notFound();
 };
