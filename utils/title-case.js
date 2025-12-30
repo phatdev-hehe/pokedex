62a3a42b7638d0657832992d37e1d1933a57a9b4
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-30 08:54:28 +0700

    386

--- utils/title-case.js
+++ utils/title-case.js
@@ -7,15 +7,17 @@ const options = {
   wordSeparators: new Set(["_", ...WORD_SEPARATORS]),
 };
 
+const terms = {
+  pokedex: "Pokédex",
+  pokemon: "Pokémon",
+};
+
 export const titleCase = (input) => {
   try {
     return split(titleCase1(input, options))
       .map((word) =>
         Number.isNaN(deromanize(word))
-          ? {
-              pokedex: "Pokédex",
-              pokemon: "Pokémon",
-            }[noCase(removeAccents(word))] ?? word
+          ? terms[noCase(removeAccents(word))] ?? word
           : word.toUpperCase()
       )
       .join(" ");
