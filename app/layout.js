13dd7c918d942b861bf79a68fd3c00d7cc72b743
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2025-12-31 18:46:24 +0700

    402

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 import { Geist } from "next/font/google";
 import { Provider as ReactWrapBalancerProvider } from "react-wrap-balancer";
 
-import "./css/globals.css";
-import FumadocsUIProvider from "./providers/fumadocs-ui-provider";
-import ProgressProvider from "./providers/progress-provider";
+import "./(layout)/css/index.css";
+import FumadocsUIProvider from "./(layout)/fumadocs-ui-provider";
+import ProgressProvider from "./(layout)/progress-provider";
 
 const font = Geist();
 
