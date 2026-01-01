5f06d4a6679a5127a0631a5ccd4428c46616d0a5
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2026-01-01 23:30:01 +0700

    411

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,14 @@ export default ({ children }) => (
   <html className={font.className} lang="en" suppressHydrationWarning>
     <body style={requiredStyles}>
       <ProgressProvider>
-        <FumadocsUIProvider>
+        <FumadocsUIProvider
+          // https://github.com/fuma-nama/fumadocs/blob/b2a9dd908814c1b5e5ae74334e55f73028ab5448/packages/ui/src/components/dialog/search-default.tsx
+          search={{
+            options: {
+              delayMs: 0,
+            },
+          }}
+        >
           <ReactWrapBalancerProvider>{children}</ReactWrapBalancerProvider>
         </FumadocsUIProvider>
       </ProgressProvider>
