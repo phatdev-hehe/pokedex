be1cc0d7a3471901ff56d455ad56912d71f0069f
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2026-01-01 15:18:02 +0700

    408

--- app/(pokedex)/[resource]/layout.js
+++ app/(pokedex)/[resource]/layout.js
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 import { Pokedex } from "@/lib/pokedex-promise-v2";
 
 // export const dynamicParams = false;
@@ -5,4 +7,7 @@ import { Pokedex } from "@/lib/pokedex-promise-v2";
 export const generateStaticParams = () =>
   Pokedex.api.routes.map((resource) => ({ resource }));
 
-export { Fragment as default } from "react";
+export default async ({ children, params }) => {
+  if (Pokedex.api.routes.includes((await params).resource)) return children;
+  else notFound();
+};
