eb0ff59e4660fbc69be788c7890661ce46456fee
phatdev-hehe <175481764+phatdev-hehe@users.noreply.github.com>
2026-01-02 16:30:25 +0700

    416

--- utils/title-case.js
+++ utils/title-case.js
@@ -1,6 +1,5 @@
 import { noCase, split } from "change-case";
 import deromanize from "deromanize";
-import { isNumber } from "es-toolkit/compat";
 import removeAccents from "remove-accents";
 import { titleCase as titleCase1, WORD_SEPARATORS } from "title-case";
 
@@ -17,9 +16,9 @@ export const titleCase = (input) => {
   try {
     return split(titleCase1(input, options))
       .map((word) =>
-        isNumber(deromanize(word))
-          ? word.toUpperCase()
-          : terms[noCase(removeAccents(word))] ?? word
+        Number.isNaN(deromanize(word))
+          ? terms[noCase(removeAccents(word))] ?? word
+          : word.toUpperCase()
       )
       .join(" ");
   } catch {}
